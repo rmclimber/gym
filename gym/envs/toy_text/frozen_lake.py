cdbd4c6f997e9d62cec9a89b4a0263d79104db20
RM <rmorris.code@gmail.com>
2022-04-10 22:53:04 -0700

    no longer printing reward as it's built

--- gym/envs/toy_text/frozen_lake.py
+++ gym/envs/toy_text/frozen_lake.py
@@ -184,7 +184,7 @@ class FrozenLakeEnv(Env):
             else:
                 reward = -.1
             # end my addition
-            print(reward)
+            # print(reward)
             return newstate, reward, done
 
         for row in range(nrow):
